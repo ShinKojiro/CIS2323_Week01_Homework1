9386882cff83b69f31e7e731e54231d5b913ff4f
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2018-08-27 01:07:44 -0500

    Initial commit

---
+++ CIS2323_Week01_Homework1.java
@@ -0,0 +1,23 @@
+public class CIS2323_Week01_Homework1 {
+    public static void main(String[] args){
+        /*System.out.println("=============================================================");
+        System.out.println("James A Hall");
+        System.out.println("");
+        System.out.println("Student Programmer, Oklahoma State University - Oklahoma City");
+        System.out.println("Delivery Driver, Mazzios Italian Eatery - Edmond");
+        System.out.println("");
+        System.out.println("12401 N MacArthur Blvd            p: (405) 229-8067");
+        System.out.println("Oklahoma City, OK 73142           e: james.a.hall@okstate.edu");
+        System.out.println("                                  g: github.com/shinkojiro");
+        System.out.println("=============================================================");*/
+
+        System.out.print("=============================================================\n"+
+        "James A Hall\n\n"+
+        "Student Programmer, Oklahoma State University - Oklahoma City\n"+
+        "Delivery Driver, Mazzios Italian Eatery - Edmond\n\n"+
+        "12401 N MacArthur Blvd            p: (405) 229-8067\n"+
+        "Oklahoma City, OK 73142           e: james.a.hall@okstate.edu\n"+
+        "                                  g: github.com/shinkojiro\n"+
+        "=============================================================");
+    }
+}
